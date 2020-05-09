62f76284d8c2f802d898bc33cbb91d542c7c33c6
licj <licj@broada.com>
2020-05-09 14:05:54 +0800

    add home页面echarts

--- hospital/hospital_admin/src/pages/home/home.jsx
+++ hospital/hospital_admin/src/pages/home/home.jsx
@@ -1,15 +1,117 @@
 import React, { Component } from "react";
-
-import { Layout } from 'antd';
+import { Layout, Card } from 'antd';
+import echarts from 'echarts/lib/echarts'
+//导入折线图
+import 'echarts/lib/chart/line'
+// 引入提示框和标题组件
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/title'
+import 'echarts/lib/component/legend'
+import 'echarts/lib/component/markPoint'
+import './index.css'
+import ReactEcharts from 'echarts-for-react'
 const { Content } = Layout;
 
 
 export default class Home extends Component {
-  
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    componentDidMount() {
+
+    }
+
+    getOption = ()=>{
+        let option = {
+            title: {
+                text: '折线图二',
+                x: 'center'
+            },
+            tooltip:{
+                trigger: 'axis'
+            },
+            xAxis: {
+                boundaryGap: false,
+                data: ['星期一','星期二','星期三','星期四','星期五','星期六','星期日']
+            },
+            yAxis: {
+                type: 'value' //数值轴，适用于连续数据
+            },
+            series : [
+                {
+                    name:'订单量',
+                    type:'line',
+                    data:[1000, 1500, 2000, 3000, 2500, 1800, 1200],
+                    areaStyle: {}
+                }
+            ]
+        }
+        return option;
+    }
+    getMonthOrder = () => {
+        let mongthOption = {
+            title: {
+                text: '月预约情况',
+                subtext: '虚构数据'
+            },
+            tooltip: {
+                trigger: 'axis'
+            },
+            legend: {
+                data: ['最高气温', '最低气温']
+            },
+            xAxis: {
+                type: 'category',
+                boundaryGap: false,
+                data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+            },
+            yAxis: {
+                type: 'value',
+                axisLabel: {
+                    formatter: '{value} °C'
+                }
+            },
+            series: [
+                {
+                    name: '最高气温',
+                    type: 'line',
+                    data: [11, 11, 15, 13, 12, 13, 10],
+                    markPoint: {
+                        data: [
+                            {type: 'max', name: '最大值'},
+                            {type: 'min', name: '最小值'}
+                        ]
+                    },
+                    markLine: {
+                        data: [
+                            {type: 'average', name: '平均值'}
+                        ]
+                    }
+                },
+            ]
+        };
+        return mongthOption;
+    }
   render () {
     return(
-      <Layout style={{height: 685 }}>
-        <Content style={{margin: 10, background: '#fff'}}>Home</Content>
+      <Layout style={{height: '100%',padding: 10 }}>
+        <Content style={{margin: 10, background: '#fff'}}>
+            <Card.Grid className="line_b">
+                <ReactEcharts option={this.getMonthOrder()}/>
+            </Card.Grid>
+            <Card.Grid className="line_b">
+                <ReactEcharts option={this.getOption()}/>
+            </Card.Grid>
+            <Card.Grid className="line_b">
+                <ReactEcharts option={this.getOption()}/>
+            </Card.Grid>
+            <Card.Grid className="line_b">
+                <ReactEcharts option={this.getOption()}/>
+            </Card.Grid>
+        </Content>
       </Layout>
     )
   }
